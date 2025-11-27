12b3367965d1459a43e952397607c8cc886418f1
simonslavik <simonslavik007@gmail.com>
2025-11-27 14:55:15 +0100

    finished auth logic

--- react-project/src/pages/home/index.jsx
+++ react-project/src/pages/home/index.jsx
@@ -1,7 +1,25 @@
+import AuthContext from '../../context';
+import { useContext } from 'react';
+
+
+    
+
+
 const Home = () => {
+    const { auth, logout } = useContext(AuthContext);
     return (
         <div>
-            Home Page
+            {auth.user ? (
+                <div>
+                    <h1>Welcome, {auth.user.name}!</h1>
+                    <p>Your role: {auth.user.role}</p>
+                    <button className='cursor-pointer border rounded hover:text-gray-500 font-semibold p-1 m-2' onClick={() => logout()}>
+                        Logout
+                    </button>
+                </div>
+            ) : (
+                <h1>Welcome to the Home Page!</h1>
+            )}
         </div>
     );
 };
